3f390bae55e67a1f059a0aa1fb50138191b92d4c
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-06-26 18:56:48 -0300

    Upload NucbazAPI

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import "dotenv/config";
+import connect from "./middlewares/connect.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 3030;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(connect);
+
+import auth from "./routes/auth.js";
+app.use("/auth", auth);
+
+import orders from "./routes/orders.js";
+app.use("/orders", orders);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
